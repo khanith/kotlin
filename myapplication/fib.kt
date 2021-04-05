dfb1548a47ce5772c16238651fec6684a40b6d1e
khanith <49386930+khanith@users.noreply.github.com>
2021-04-05 09:01:50 +0530

    Update fib.kt

--- myapplication/fib.kt
+++ myapplication/fib.kt
@@ -1,13 +1,13 @@
 package com.example.myapplication
 
-import java.util.*
-fun main()
-{
+import java.util.*;
+fun main(args: Array<String>) {
     var sc=Scanner(System.`in`)
-    println("Enter no.of terms that we want in fibonacci")
+    println("Enter no.of digits")
     var n=sc.nextInt()
     var r1=0
     var r2=1
+    var i=1
     if(n==1)
     {
         println(0)
@@ -17,13 +17,18 @@ fun main()
         println(r1)
         println(r2)
     }
-    else {
-        for (i in 1..n-2) {
-            print("$r1 + ")
-
-            val sum = r1 + r2
-            r1 = r2
-            r2 = sum
+    else
+    {
+        println(r1)
+        println(r2)
+        while(i<=n-2)
+        {
+            var sum=r1+r2
+            println(sum)
+            r1=r2
+            r2=sum
+            i++
         }
+
     }
 }
